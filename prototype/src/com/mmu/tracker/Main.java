573cf55f9ef4ba44172b2dfb3ffbf1c7958f0c42
Seirra <sophietheopossum@yandex.ru>
2021-03-22 21:13:50 +0000

    improve prototype readability and improve naming

--- prototype/src/com/mmu/tracker/Main.java
+++ prototype/src/com/mmu/tracker/Main.java
@@ -1,18 +1,17 @@
 package com.mmu.tracker;
-
+//import required libraries
 import javax.swing.*;
 
-public class mainForm {
-    private JPanel mainPanel;
-    private JTextField textField1;
+public class Main {
+    //add form items
     private JButton goButton;
-
-    public mainForm() {
-    }
-
+    private JPanel mainPanel;
+    private JTextField searchBar;
+    //main code block
     public static void main(String[] args) {
+        //load and display the form
         JFrame frame = new JFrame("COVIDASH");
-        frame.setContentPane(new mainForm().mainPanel);
+        frame.setContentPane(new Main().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
