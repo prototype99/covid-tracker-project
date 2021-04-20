95328a4d27f3d5793396b28ce0e3ac96442306f9
Seirra <sophietheopossum@yandex.ru>
2021-04-20 23:26:47 +0100

    fill out the constructor

--- prototype/src/com/mmu/tracker/CovidRecord.java
+++ prototype/src/com/mmu/tracker/CovidRecord.java
@@ -4,10 +4,20 @@ package com.mmu.tracker;
 import kong.unirest.json.JSONObject;
 
 public class CovidRecord {
-    String countryCode, countryExternalName, countryInternalName;
+    String countryCode, countryExternalName, countryInternalName, date;
     int caseAll, caseNew, deathAll, deathNew, recoveryAll, recoveryNew;
     //case = confirmed - (death + recovery )
     CovidRecord(JSONObject j){
-        
+        countryCode = j.getString("CountryCode");
+        countryExternalName = j.getString("Country");
+        countryInternalName = j.getString("Slug");
+        date = j.getString("Date");
+        deathAll = j.getInt("TotalDeaths");
+        deathNew = j.getInt("NewDeaths");
+        recoveryAll = j.getInt("TotalRecovered");
+        recoveryNew = j.getInt("NewRecovered");
+        //we have to calculate these because it is not provided by default
+        caseAll = j.getInt("TotalConfirmed") - (deathAll + recoveryAll);
+        caseNew = j.getInt("NewConfirmed") - (deathNew + recoveryNew);
     }
 }
