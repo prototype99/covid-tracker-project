d2b5cb7e551251550fd9463ea35c082ad19b91dd
Seirra <sophietheopossum@yandex.ru>
2021-04-21 13:03:38 +0100

    make it possible to populate combobox

--- prototype/src/com/mmu/tracker/Main.java
+++ prototype/src/com/mmu/tracker/Main.java
@@ -4,10 +4,9 @@ import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import kong.unirest.HttpResponse;
 import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONException;
 import kong.unirest.json.JSONObject;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 public class Main {
     static ArrayList<CovidRecord> data;
@@ -19,20 +18,14 @@ public class Main {
     /*combobox code inspired by https://kodejava.org/how-do-i-set-and-get-the-selected-item-in-jcombobox/
     code for form interactions*/
     public Main() {
+        //equivalent to Unirest.setTimeouts(0, 0); in older unirest-java
+        Unirest.config().socketTimeout(0).connectTimeout(0);
+        //we load the data here to avoid hell
+        loadData(searchBar);
         //just a basic listener for now to prove it's possible
-        searchBar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                System.out.println(searchBar.getSelectedItem());
-            }
-        });
+        searchBar.addActionListener(actionEvent -> System.out.println(searchBar.getSelectedItem()));
         //refresh data in case it's too old
-        refreshButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                loadData();
-            }
-        });
+        refreshButton.addActionListener(actionEvent -> loadData(searchBar));
     }
     //main code block
     public static void main(String[] args) {
@@ -42,9 +35,6 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        //equivalent to Unirest.setTimeouts(0, 0); in older unirest-java
-        Unirest.config().socketTimeout(0).connectTimeout(0);
-        loadData();
     }
     //future function to validate user input
     static boolean isValid(){
@@ -62,9 +52,16 @@ public class Main {
         //we must use getbody to get the part we care about
         return response.getBody();
     }
-    static void loadData(){
-        //get data
-        JSONArray top = download("summary").getObject().getJSONArray("Countries");
+    static void loadData(JComboBox<String> searchBar){
+        //make sure variable is accessible
+        JSONArray top;
+        try {
+            //get data
+            top = download("summary").getObject().getJSONArray("Countries");
+        } catch (JSONException j) {
+            System.out.println("json parsing failed, is data correct?");
+            top = new JSONArray();
+        }
         //initialise array
         data  = new ArrayList<>();
         //iterate through the countries
@@ -85,9 +82,9 @@ public class Main {
                     j.getString("Date"), deathAll, deathNew, recoveryAll, recoveryNew,
                     (j.getInt("TotalConfirmed") - (deathAll + recoveryAll)), (j.getInt(
                             "NewConfirmed") - (deathNew + recoveryNew))));
-//            searchBar.addItem(countryCode);
-//            searchBar.addItem(countryExternalName);
-//            searchBar.addItem(countryInternalName);
+            searchBar.addItem(countryCode);
+            searchBar.addItem(countryExternalName);
+            searchBar.addItem(countryInternalName);
         }
     }
     //test function to review output. do not start apirequest with a slash!
